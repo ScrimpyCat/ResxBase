dacb4690f81bb89a8e498812874a858e75bb7aff
Stefan Johnson <sj@scrimpycat.io>
2019-06-10 22:56:51 +1000

    Create encoders

--- lib/resx_base.ex
+++ lib/resx_base.ex
@@ -1,2 +1,33 @@
 defmodule ResxBase do
+    require Itsy.Binary
+
+    "0123456789ABCDEF"
+    |> String.graphemes
+    |> Enum.with_index
+    |> Itsy.Binary.encoder(encode: :encode16_upper, decode: :decode16_upper, docs: false)
+
+    "0123456789abcdef"
+    |> String.graphemes
+    |> Enum.with_index
+    |> Itsy.Binary.encoder(encode: :encode16_lower, decode: :decode16_lower, docs: false)
+
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
+    |> String.graphemes
+    |> Enum.with_index
+    |> Itsy.Binary.encoder(encode: :encode32, decode: :decode32, docs: false)
+
+    "0123456789ABCDEFGHIJKLMNOPQRSTUV"
+    |> String.graphemes
+    |> Enum.with_index
+    |> Itsy.Binary.encoder(encode: :hex_encode32, decode: :hex_decode32, docs: false)
+
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+    |> String.graphemes
+    |> Enum.with_index
+    |> Itsy.Binary.encoder(encode: :encode64, decode: :decode64, docs: false)
+
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
+    |> String.graphemes
+    |> Enum.with_index
+    |> Itsy.Binary.encoder(encode: :url_encode64, decode: :url_decode64, docs: false)
 end
